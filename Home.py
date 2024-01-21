fbbe8c2cdbca7772ee9ee1ce174e5295c5d09d15
Alex Holzbach <ajholzbach>
2024-01-21 12:50:20 -0600

    oops

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(page_title="Datathon 2024 Team Well, Well, Well", layout="wide")
 st.title('Well, Well, Well')
 st.markdown("## 2024 Datathon Submission, Chevron Track")
-st.markdown("### Team Members: Alex Holzbach, Arnav Brurdgunte, Chuk Uzowihe, and Evan Stegall")
+st.markdown("### Team Members: Alex Holzbach, Arnav Bururdgunte, Chuk Uzowihe, and Evan Stegall")
 st.markdown("#### [Github Repo](https://github.com/ajholzbach/Datathon_2024)")
 st.sidebar.header("Navigation")
 wellwellwell = Image.open('Figures/wellwellwell.png')
